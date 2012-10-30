acc88d699d0c826d55b10af19a47429d1ab90027
Ren Quinn <ren@rocketmade.com>
2012-10-30 15:48:32 -0600

    Start basic design for nodes and messages

--- paxos.js
+++ paxos.js
@@ -1,3 +1,34 @@
 $(function() {
-  alert("Hello");
+  /*
+   * jQuery QTIP
+   * http://craigsworks.com/projects/qtip/docs/tutorials/
+   */
+
+  $('.node').qtip({
+    content: {
+      url: 'nodeinfo.html'
+    },
+    position: {
+      corner: {
+        target: 'rightMiddle',
+        tooltip: 'leftMiddle'
+      }
+    },
+    show: 'mouseover',
+    hide: 'mouseout'
+  })
+
+  $('.message').qtip({
+    content: {
+      url: 'messageinfo.html'
+    },
+    position: {
+      corner: {
+        target: 'rightMiddle',
+        tooltip: 'leftMiddle'
+      }
+    },
+    show: 'mouseover',
+    hide: 'mouseout'
+  })
 });
