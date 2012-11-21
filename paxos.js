42d75c5a8f12cb0b22833950c8af904d354369ae
Ren <renquinn@gmail.com>
2012-11-20 17:14:47 -0700

    Improve design, switch from qtip to bootstraps popover

--- paxos.js
+++ paxos.js
@@ -4,6 +4,7 @@ $(function() {
    * http://craigsworks.com/projects/qtip/docs/tutorials/
    */
 
+/*
   $('.node').qtip({
     content: {
       url: 'nodeinfo.html'
@@ -31,4 +32,29 @@ $(function() {
     show: 'mouseover',
     hide: 'mouseout'
   })
+*/
+
+  $('.node').popover({
+    placement: 'bottom',
+    trigger: 'hover',
+    html: true,
+    title: function() {
+      return $(this).data('type');
+    },
+    content: function() {
+      // Determine the node type
+      // TODO: Insert the correct data into the table
+      return "\
+        <table>\
+          <tr>\
+            <th>A</th>\
+          </tr>\
+          <tr>\
+            <td>B</td>\
+          </tr>\
+        </table>";
+    }
+    //template: '<div class="popover"><div class="arrow"></div><div class="popover-inner"><h3 class="popover-title">TITLE!!!!</h3><div class="popover-content"><p>CONTENT!</p></div></div></div>'
+  });
+
 });
